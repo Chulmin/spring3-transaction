1e87855fc1bd2b5a3733533e7a8a31076b700d53
chulmin.kim <gkcm982@gmail.com>
2013-01-18 18:03:23 +0900

    테스트 Set 작성중.

---
+++ src/main/java/com/javaforweb/controller/TransactionTestController.java
@@ -0,0 +1,8 @@
+package com.javaforweb.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TransactionTestController {
+
+}
---
+++ src/main/java/com/javaforweb/service/AService.java
@@ -0,0 +1,5 @@
+package com.javaforweb.service;
+
+public interface AService {
+
+}
